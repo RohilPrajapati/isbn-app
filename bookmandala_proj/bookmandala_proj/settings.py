b582a5ac789fa15418c9b9ca25841653c623ba33
RohilPrajapati <rohilprajapati@gmail.com>
2025-10-11 22:23:29 +0545

    fix issue with local build as enviroment variable name issue

--- bookmandala_proj/bookmandala_proj/settings.py
+++ bookmandala_proj/bookmandala_proj/settings.py
@@ -136,3 +136,13 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '1/day',
+    }
+}
